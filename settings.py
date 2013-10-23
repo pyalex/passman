a2e044a5986240a78f7730a67f6877cdaf10a935
pyalex <moskalenko.alexey@gmail.com>
2013-10-23 14:39:43 +0000

    Settings update

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
-MONGO_HOST = '127.0.0.1'
-MONGO_PORT = 27017
+MONGO_HOST = 'pwm-p-mng-s1r1.pwm.loc'
+MONGO_PORT = 27018
 MONGO_NAME = 'passman'
 SERVER_PORT = 8080
 
-AMQP_HOST = 'localhost'
+AMQP_HOST = 'pwm-p-rabbit01.pwm.loc'
 AMQP_PORT = 5672
